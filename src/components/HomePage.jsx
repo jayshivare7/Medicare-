b36dc0ea12d7ba4b98a99cbbf559e34911f608fd
Jay Shivare <110019310+jayshivare7@users.noreply.github.com>
2024-08-02 01:57:30 +0530

    123

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import { NavLink } from "react-router-dom";
 import medicare from './medicare.png'
-
+import doc from './doc.png'
+import kif from './kif.png'
 function HomePage() {
   return (
     <div className="home-page">
@@ -11,7 +12,7 @@ function HomePage() {
        <div className='fields'>
        <Router>
       
-        <nav>
+        <nav style={{width:'99vw'}}>
           <Link to="/symptom-checker">Symptom Checker</Link>
           <Link to="/health-schemes">Health Schemes</Link>
           <Link to="/medical-records">Medical Records</Link>
@@ -23,6 +24,19 @@ function HomePage() {
     </div>
 
     </div>
+    <div style={{height:'30px'}}>
+
+    </div>
+    <div>
+        <img src={doc} alt="Doctor" style={{ height: '80vh', width: '99vw' }} />
+      </div>
+      <div style={{height:'100px'}}>
+
+    </div>
+    <div>
+        <img src={kif} alt="Doctor" style={{ height: '80vh', width: '99vw' }} />
+      </div>
+       
 
     </div>
   );
